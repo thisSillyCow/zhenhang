f1e27a98ab136c7fe55891af39a4b69727bfa48b
zhenhang <1123978021@qq.com>
2018-06-10 21:57:31 +0800

    first commit

---
+++ pages/Index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page-index">
+        <h2>LAVAS</h2>
+        <h4>1111111111</h4>
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use( ElementUI);
+export default {
+    name: 'index',
+};
+</script>
+
+<style lang="stylus" scoped>
+
+.page-index
+    display flex
+    justify-content center
+    align-items center
+    flex-direction column
+
+    h2
+        font-size 46px
+        font-weight 500
+        margin-bottom 0
+
+</style>
